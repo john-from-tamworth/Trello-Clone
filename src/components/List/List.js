d83abf7ce87ab88ed05b5310e5ecbc2abd76fb65
John Hands <johnhands@Johns-MacBook-Pro.local>
2021-01-08 23:55:45 +0000

    initial commmit todo list and todo title

---
+++ src/components/List/List.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { CssBaseline, Paper, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Title from './Title';
+
+const useStyle = makeStyles((theme) => ({
+  root: {
+    width: '300px',
+    backgroundColor: '#dbdbdb',
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+function List() {
+  const classes = useStyle();
+  return (
+    <div>
+      <Paper className={classes.root}>
+        <CssBaseline />
+        <Title />
+      </Paper>
+    </div>
+  );
+}
+
+export default List;
